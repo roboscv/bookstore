a08a483e10921c17c92e34b173b89a4629ac49ca
Jamie Cheng <jacheng@vt.edu>
2014-09-29 23:42:37 -0400

    class 4 lab 4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
 		if @book.save
-			redirect_to @book
+			redirect_to @book, notice: "#{@book.title} was created!"
 		else 
 			render :new
 		end
@@ -23,7 +23,7 @@ class BooksController < ApplicationController
 
 	def update
 		if @book.update(book_params)
-			redirect_to @book
+			redirect_to @book, notice: "#{@book.title} was created!"
 		else
 			render :new
 		end
