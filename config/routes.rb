9d294dad0cee549d7f382d4820050bdfa3843f6a
casey_frederick <1deaged@gmail.com>
2014-10-02 20:02:01 -0400

    lab 4 class 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Bookstore::Application.routes.draw do
-  resources :books do 
-    get 'page/:page', :action => :index, :on => :collection
-  end
-  root 'books#index'
+  resources :reviews
+
+  resources :books do
+  resources :reviews
+  get 'page/:page', :action => :index, :on => :collection
+end
+root 'books#index'
 
 
 
